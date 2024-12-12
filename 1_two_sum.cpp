6f394ec4ab75cda769c893b6ff6ce489aafe3da7
Roland Sárosy <36110029+rolandsarosy@users.noreply.github.com>
2024-12-12 16:17:49 +0100

    Update 1_two_sum.cpp

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -18,3 +18,24 @@ public:
         throw invalid_argument("Function was supplied with invalid inputs as there always must be exactly one correct solution.");
     }
 };
+
+
+// O(n) solution. Lesson: It's hashmaps. It always is.
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> traversalMap; // Map: <value, index>
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complementerValue = target - nums[i]; // We are looking for this value
+
+            if (traversalMap.find(complementerValue) != traversalMap.end()) { // Lookup if map has key of COMPLEMENTER
+                return {i, traversalMap[complementerValue]};
+            } else {
+                traversalMap[nums[i]] = i;
+            }
+        }
+        
+        throw invalid_argument("Invalid state, shouldn't have been reachable.");
+    }
+};
