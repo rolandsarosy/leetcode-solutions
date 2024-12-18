b50b3becd70d57562c3d1ef4d449db5c87bc019d
Roland Sárosy <36110029+rolandsarosy@users.noreply.github.com>
2024-12-18 13:59:11 +0100

    Update 121_best_time_to_buy_and_sell_stock.kt

--- 121_best_time_to_buy_and_sell_stock.kt
+++ 121_best_time_to_buy_and_sell_stock.kt
@@ -18,3 +18,28 @@ class Solution {
         return currentMaxProfit
     }
 }
+
+// O(n) two pointers approach. :) 
+class Solution {
+    fun maxProfit(prices: IntArray): Int { 
+        if (prices.size == 1) return 0
+        
+        var currentMaxProfit = 0;
+        var left = 0; // Buying value Pointer
+        var right = 1; // Selling value Pointer
+
+        for (i in prices.indices - 1) { 
+            if (prices[right] < prices[left]) {
+                left = right
+                right++
+                continue
+            }
+
+            val potentialProfit = prices[right] - prices[left] 
+            right++
+            if (potentialProfit > currentMaxProfit) currentMaxProfit = potentialProfit 
+        }
+
+        return currentMaxProfit;
+    }
+}
