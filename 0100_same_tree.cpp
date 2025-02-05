2ad1b07c24f11394f15e6f3e7c9d30101b35ab35
Roland Sárosy <36110029+rolandsarosy@users.noreply.github.com>
2025-02-05 12:57:58 +0100

    Update 0100_same_tree.cpp

--- 0100_same_tree.cpp
+++ 0100_same_tree.cpp
@@ -50,3 +50,27 @@ public:
         return true;
     }
 };
+
+// Recursive DFS solution
+// Time Complexity: O(min(n, m)) where n and m represent the amount of nodes in p and q trees. Could be argued for O(n+m) but we traverse the trees simultaneously.
+// Space Complexity: O(max(n_h, m_h)) where n_h and m_h represent the height of p and q trees respectively.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
